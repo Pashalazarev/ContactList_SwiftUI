5a739e3e73e543a5d060da62314fead6cde053e4
Pashalazarev <mymessagebox9@gmail.com>
2022-11-01 20:46:11 +0300

    Initial Commit

---
+++ ContactList_SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList_SwiftUI
+//
+//  Created by Pavel Lazarev Macbook on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
